1521736c1fa421ead6754717a10a8209c91124f7
Pao21-creator <mendozapaolagus@gmail.com>
2025-03-20 08:55:05 -0300

    Update LegendControl.jsx

--- src/components/nieve/LegendControl.jsx
+++ src/components/nieve/LegendControl.jsx
@@ -2,8 +2,9 @@ import React, { useEffect } from 'react';
 import { useMap } from 'react-leaflet';
 import L from 'leaflet';
 import '../../styles/nieve/Mapa.css';
+import '../../styles/nieve/LegendControl.css';
 
-function LegendControl({year}) {
+function LegendControl({ year }) {
   const map = useMap(); // Usamos useMap para acceder al mapa de Leaflet
 
   useEffect(() => {
@@ -14,6 +15,9 @@ function LegendControl({year}) {
       const div = L.DomUtil.create('div', 'info legend'); // Crear un contenedor para la leyenda
       const grades = [-1, 40, 80, 120, 160, 200];  // Los valores de los colores
       const labels = ['#eff6f0', '#acdcec', '#68bcf8', '#3480fc', '#121bd1'];  // Paleta de colores
+
+      div.innerHTML +=  '<div class="legend-title">Referencias</div>';
+
 
       // Agregar cada color y su valor correspondiente a la leyenda
       for (let i = 0; i < grades.length - 1; i++) {
@@ -35,8 +39,28 @@ function LegendControl({year}) {
     if (mapContainerYear && !mapContainerYear.querySelector('.fixed-text')) {
       const fixedTextYear = document.createElement('div');
       fixedTextYear.classList.add('fixed-text'); // Añadir la clase para estilo
-      fixedTextYear.innerHTML = 'Promedio '+`${year}`; // El texto fijo
+      fixedTextYear.innerHTML = 'Promedio ' + `${year}`; // El texto fijo
       mapContainerYear.appendChild(fixedTextYear);
+
+      // Crear y agregar un tooltip de instrucciones al pasar el mouse sobre el contenedor
+      const tooltip = L.tooltip({
+        permanent: false,  // No se mantiene visible permanentemente
+        direction: 'top',
+        offset: [0, -10],
+        className: 'tooltip-custom',
+      })
+        .setContent('Seleccione un año en el selector de la izquierda para cambiar el promedio.') // Texto de la instrucción
+        .setLatLng(map.getCenter()); // Ubicar el tooltip cerca del centro del mapa o en las coordenadas deseadas
+
+      // Mostrar el tooltip al pasar el mouse sobre el contenedor de mapYear
+      mapContainerYear.addEventListener('mouseover', () => {
+        tooltip.addTo(map);  // Mostrar el tooltip
+      });
+
+      // Eliminar el tooltip cuando el mouse sale del contenedor
+      mapContainerYear.addEventListener('mouseout', () => {
+        tooltip.remove();  // Ocultar el tooltip
+      });
     }
 
     if (mapContainerProm && !mapContainerProm.querySelector('.fixed-text')) {
@@ -63,9 +87,10 @@ function LegendControl({year}) {
         }
       }
     };
-  }, [map]);
+  }, [map, year]);
 
   return null;
 }
 
 export default LegendControl;
+
